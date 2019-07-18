e529901ae914f40c0157768fe2648059b023cfb1
Kirat24 <51926765+Kirat24@users.noreply.github.com>
2019-07-17 22:51:07 -0400

    Update host_usage.sh

--- scripts/host_usage.sh
+++ scripts/host_usage.sh
@@ -1,23 +1,14 @@
 #! /bin/bash
-
-
 psql_host=localhost
 port=5432 
 db_name=host_agent
 username=postgres
 password=password
-
-
 get_free_mem(){
 free_mem=$(vmstat -s | egrep "free memory$" | awk '{print $1}')
-
 }
-
 get_cpu_idel(){
-cpu_idel=$(vmstat | awk '{for(i=NF;i>0;i--)if($i=="id"){x=i;break}}END {print $x}')
-
-}
-
+cpu_idel=$(vmstat | awk '{for(i=NF;i>0;i--)if($i=="id"){x=i;break}}END {print $x}')}
 get_disk_IO(){
 disk_IO=$(vmstat -d | awk '{for(i=NF;i>0;i--)if($i=="cur"){x=i+1;break}}END {print $x}')
 }
@@ -26,15 +17,11 @@ disk_avail=$(df /dev/sda1 -m | awk '{for(i=NF;i>0;i--) if($i=="Available"){x=i;b
 }
 get_cpu_kernel(){
 cpu_kernel=$(vmstat | awk '{for(i=NF;i>0;i--)if($i=="sy"){x=i;break}}END {print $x}')
-
 }
-
 get_host_id(){
-
 host_id=$(cat /home/centos/dev/jrvs/bootcamp/host_agent/scripts/host_info_id)
 }
 timestamp=$(vmstat -t | awk '{for(i=NF;i>0;i--)if($i=="UTC"){x=i;break}}END {print $x  " " $(x+1)}')
-
 get_free_mem
 get_host_id
 get_cpu_idel
@@ -46,9 +33,6 @@ insert_stmt=$(cat <<-END
 INSERT INTO  host_usage( timestamp, host_id ,free_mem,cpu_idel,cpu_kernel,disk_io,disk_avail) VALUES('${timestamp}','${host_id}','${free_mem}','${cpu_idel}','${cpu_kernel}','${disk_IO}','${disk_avail}');
 END
 )
- echo $insert_stmt
-
+echo $insert_stmt
 export PGPASSWORD=$password
 psql -h $psql_host -p $port -U $username -d $db_name -c "$insert_stmt"
-
-
